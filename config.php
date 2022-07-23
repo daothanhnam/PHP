75904cce6e2247561a97d375cdb72f04acd3c6b7
Daothanhnam <88824678+daothanhnam@users.noreply.github.com>
2022-07-23 10:20:20 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dataHost = 'localhost';
-    $dataName = 'stu_crud';
-    $dataUserName = 'root';
-    $dataPassword = '';
-
-    $mysqli = mysqli_connect($dataHost, $dataUserName, $dataPassword, $dataName);
-?>
